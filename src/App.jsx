6ec516014d799b02606bba28a98325b969057e07
seerat makhija <56536825+seerat1010@users.noreply.github.com>
2020-04-28 13:49:49 +0530

    taskbar

---
+++ src/App.jsx
@@ -0,0 +1,24 @@
+import React from "react";
+import "./styles.css";
+
+export default function App() {
+  return (
+    <div class="app">
+      <ul>
+        <li>
+          <a href="#">Home</a>
+        </li>
+        <li>
+          <a href="#">About</a>
+        </li>
+        <li>
+          <a href="#">Contact</a>
+        </li>
+       <li class="b"><button>Login</button></li>
+       <li class="b"><button>Sign Up</button></li>
+      </ul>
+      
+ 
+    </div>
+  );
+}
