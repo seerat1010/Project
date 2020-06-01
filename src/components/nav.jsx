55a85ea2237465394d064b8980c8a604acd08895
seerat makhija <56536825+seerat1010@users.noreply.github.com>
2020-06-01 15:14:20 +0530

    loader

--- src/components/nav.jsx
+++ src/components/nav.jsx
@@ -4,11 +4,10 @@ import './styles.css';
 function Navi(){
 return (
 <div class="app">
-
  <div class="nav">    {/* this div is made so that we can use sticky position collectively on list and buttons*/ }
    <ul>
     <li><a href="#">Home</a></li>
-    <li><a href="#">About</a></li>
+    <li><a href="#here">About</a></li>
     <li><a href="#">Contact</a></li>
       <div class="b"> 
         <button class="button"> <a href="#">Login</a></button>
@@ -16,10 +15,21 @@ return (
       </div>
   </ul>
  </div>
-
+ <div class="whole">
  <img className="image" src="../images/h.jpg" />
  <h1 className="a"> Welcome To </h1>
  <h1 className="a"> Signature Stays</h1>
+ <div id="here" >
+   <h1>header</h1>
+   <h1>header</h1>
+   <h1>header</h1>
+   <h1>header</h1>
+   <h1>header</h1>
+   <h1>header</h1>
+   <h1>header</h1>
+   
+    </div>
+    </div>
 </div>
 );
 }
